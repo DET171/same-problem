61e27c5a8648aeaf269af30cf42da36f89697656
DET171 <68373699+DET171@users.noreply.github.com>
2020-11-29 10:52:53 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
-function c () {
+function z () {
     OLED.writeStringNewLine("HELP ME")
 }
-function b () {
-    OLED.writeStringNewLine("REQUESTING FOR DRINKS")
-}
-function a () {
+function y () {
     OLED.writeStringNewLine("REQUESTING FOR FOOD")
+}
+function x () {
+    OLED.writeStringNewLine("REQUESTING FOR DRINKS")
 }
 OLED.init(128, 64)
 basic.forever(function () {
     basic.showString("PRESS A FOR FOOD B FOR DRINKS C FOR HELP")
-    // prees A!
+    // press A!
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
-        a()
+        y()
     }
     // press B!
     if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P0)) {
-        b()
+        x()
     }
     if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P0)) {
-        c()
+        z()
     }
     // else...
     if (tinkercademy.ADKeyboard(ADKeys.D, AnalogPin.P0)) {
